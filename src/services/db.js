da826903b0ed628a014fdde77df88c206cc0767d
Guillaume Duhan <myhappyagency@gmail.com>
2020-03-07 11:12:21 +0100

    service db added

---
+++ src/services/db.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+// Get a Firestore instance
+export const db = firebase
+.initializeApp({
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DB_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+})
+.firestore()
+
+// Export types that exists in Firestore
+// This is not always necessary, but it's used in other examples
+const { Timestamp, GeoPoint } = firebase.firestore
+export { Timestamp, GeoPoint }
+
+// if using Firebase JS SDK < 5.8.0
