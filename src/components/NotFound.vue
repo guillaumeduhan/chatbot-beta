ed058e265617743ef14de74cbca94c5ef0538da5
halma <myhappyagency@gmail.com>
2020-03-11 11:17:17 +0100

    mouse

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,14 +1,28 @@
 <template lang="pug">
   #NotFound
-    h1 Error 404 - not found.
+    .container.d-flex.flex-column.align-items-center.justify-content-center
+      img#mouse(:src="mouse")
+      p {{ message }}
 </template>
 
 <script>
+import mouse from '../assets/mouse.png'
+
 export default {
   name: 'NotFound',
+  data() {
+    return {
+      mouse,
+      message: 'Le chat n\'est pas là, les souris dansent.'
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-#NotFound {}
+#NotFound {
+  #mouse {
+    max-width: 300px;
+  }
+}
 </style>
