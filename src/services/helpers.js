65f36bfdf3475d7a2ccdeba693316c13f2b8c621
Guillaume Duhan <myhappyagency@gmail.com>
2020-04-05 22:38:06 +0200

    helpers update

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -2,6 +2,7 @@
   * Helpers
   */
 
+// eslint-disable-next-line import/prefer-default-export
 export const idGenerator = () => {
-  return "S0" + (Math.random().toString(6).substr(2, 6)).toUpperCase()
+  return `S0${(Math.random().toString(6).substr(2, 6)).toUpperCase()}`
 }
